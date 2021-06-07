dfa91a43436ab7d9f41fa8cd3538a99db11e9ceb
sibille <sibille@users.noreply.github.com>
2021-06-07 11:01:10 +0200

    Add project files.

---
+++ SampleInstaller/SolutionWizard.cs
@@ -0,0 +1,102 @@
+﻿using EnvDTE;
+using Microsoft.Internal.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TemplateWizard;
+using Microsoft.VisualStudio.Threading;
+using SampleUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleInstaller
+{
+    public class SolutionWizard : IWizard
+    {
+
+        private readonly AsyncLazy<IVsUIShell> _uiShell = new AsyncLazy<IVsUIShell>(
+            async () =>
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                return ServiceProvider.GlobalProvider.GetService(typeof(SVsUIShell)) as IVsUIShell;
+            },
+            ThreadHelper.JoinableTaskFactory);
+
+        protected void Initialize()
+        {
+
+                
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+
+        }
+
+
+
+          
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+
+            if (runKind == WizardRunKind.AsNewProject || runKind == WizardRunKind.AsMultiProject)
+            {
+                ShowModal();
+            }
+
+        }
+
+            
+
+        public SolutionWizard()
+        {
+        }
+
+        public void ShowModal()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                // get the owner of this dialog
+                var uiShell = await _uiShell.GetValueAsync();
+                uiShell.GetDialogOwnerHwnd(out IntPtr hwnd);
+
+                var dialog = new MainWindow();
+                dialog.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterOwner;
+
+                uiShell.EnableModeless(0);
+
+                try
+                {
+                    WindowHelper.ShowModal(dialog, hwnd);
+                }
+                finally
+                {
+                    // This will take place after the window is closed.
+                    uiShell.EnableModeless(1);
+                }
+            });
+
+    }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+ }
---
+++ SampleUI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Items.Add("1");
+            Items.Add("2");
+        }
+
+        public ObservableCollection<string> Items { get; private set; } = new ObservableCollection<string>();
+
+    }
+
+}
